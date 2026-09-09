7c83c000aa9f0339512e38d54748124c5e9ddf44
nithyasreerr2348-lgtm <nithyasreerr2348@gmail.com>
2026-09-09 20:12:09 +0530

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -292,9 +292,10 @@ def analyze_video():
 # ==========================================
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 7860))
 
     app.run(
         host="0.0.0.0",
-        port=7860,
+        port=port,
         debug=False
     )
